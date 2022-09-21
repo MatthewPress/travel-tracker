3ad95b2f33a24de0e1bd225faff9babd2d96b55f
MatthewPress <press.matt14@gmail.com>
2022-09-21 01:19:04 -0600

    Feat: add get request and display data

--- src/scripts.js
+++ src/scripts.js
@@ -1,11 +1,64 @@
-// This is the JavaScript entry file - your code begins here
-// Do not delete or rename this file ********
-
-// An example of how you tell webpack to use a CSS (SCSS) file
+// DEPENDENCIES **************************************
 import './css/styles.css';
+// import './images/turing-logo.png'
+import Traveler from './Traveler';
+import Repository from './Repository';
 
-// An example of how you tell webpack to use an image (also need to link to it in the index.html)
-import './images/turing-logo.png'
+// GLOBAL DATA ***************************************
+let travelersRepo;
+let destinationsRepo;
+let tripsRepo;
+let user;
+let currentDate;
+
+// FETCH DATA ****************************************
+function requestData(id) {
+  Promise.all([getData(`travelers/${id}`), getData('trips'), getData('destinations')])
+    .then((data) => {
+      setData(data);
+    });
+}
+
+function getData(path) {
+  return fetch(`http://localhost:3001/api/v1/${path}`)
+    .then(response => {
+      return response.json();
+    })
+}
+
+requestData(50);
+
+function setData(data) {
+  destinationsRepo = new Repository(data[2].destinations);
+  tripsRepo = new Repository(data[1].trips);
+  user = new Traveler(data[0]);
+  user.setTrips(tripsRepo);
+  console.log("user", user);
+
+  displayUserInfo();
+}
+// DOM ELEMENTS **************************************
+const tripsSection = document.querySelector('.trips-section');
+const yearlyExpenseDisplay = document.querySelector('.yearly-expense-display');
+
+// EVENT LISTENERS ***********************************
 
 
-console.log('This is the JavaScript entry file - your code begins here.');
+
+// EVENT HANDLERS ************************************
+function displayUserInfo() {
+  tripsSection.innerHTML = "";
+
+  user.trips.forEach(trip => {
+    tripsSection.innerHTML += `
+    <article>
+      <p>Destination: <span class="trip-destination-display">${destinationsRepo.filterData('id', trip.destinationID)[0].destination}</span></p>
+      <p>Duration: <span class="trip-duration-display">${trip.duration}</span> days</p>
+      <p>Status: <span class="trip-status-display">${trip.status}</span></p>
+      <p>Date: <span class="trip-date-display">${trip.date}</span></p>
+    </article>
+    `;
+  })
+
+  yearlyExpenseDisplay.innerText = `$${user.calcYearExpenses(destinationsRepo, 2022)}`;
+}
