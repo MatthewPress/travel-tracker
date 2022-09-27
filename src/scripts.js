17039c4e757c15a19a04b4b63ba761826e412056
MatthewPress <press.matt14@gmail.com>
2022-09-27 00:09:39 -0600

    Fix: correct an issue with finding trip prices

--- src/scripts.js
+++ src/scripts.js
@@ -6,7 +6,6 @@ import Traveler from './Traveler';
 import Repository from './Repository';
 import { getData, postData } from './apiCalls';
 import dayjs from 'dayjs';
-// import './images/turing-logo.png'
 
 // GLOBAL DATA ***************************************
 let currentDate = dayjs();
@@ -22,7 +21,7 @@ function requestData(id) {
       setData(data);
     })
     .catch(error => {
-      alert('promise error');
+      console.log('A method in setData probably fucked up');
     });
 }
 
@@ -40,6 +39,7 @@ function setData(data) {
   parseTripDates();
   displayUserTrips();
   populateLocationChoices();
+  validateTripRequest();
 }
 
 // DOM ELEMENTS **************************************
@@ -52,17 +52,27 @@ const resetBtn = document.querySelector('.reset');
 
 const mainDisplay = document.querySelector('main');
 
+const destinationNameDisplay = document.querySelector('.destination-name-display');
+const destinationImg = document.querySelector('#destination-img');
+const destinationLodgingDisplay = document.querySelector('.destination-lodging-display');
+const destinationFlightDisplay = document.querySelector('.destination-flight-display');
+
 const requestTripForm = document.forms.planTrip;
 const dateInput = document.querySelector('#date-input');
 const locationChoices = document.querySelector('.location-choices');
+const estimatedCostDisplay = document.querySelector('.estimated-cost-display');
 const requestTripBtn = document.querySelector('.request-trip-btn');
 
-const tripsSection = document.querySelector('.user-trips-section');
+const userTripsDisplay = document.querySelectorAll('.user-trips-display');
+const pastTripsSection = document.querySelector('.past');
+const futureTripsSection = document.querySelector('.future');
+const pendingTripsSection = document.querySelector('.pending');
+
 const yearlyExpenseDisplay = document.querySelector('.yearly-expense-display');
 
-
 // EVENT LISTENERS ***********************************
-window.addEventListener('load', checkForData);
+// window.addEventListener('load', checkForData);
+loginForm.addEventListener('input', verifyLogin);
 loginBtn.addEventListener('click', login);
 requestTripForm.addEventListener('change', validateTripRequest);
 requestTripBtn.addEventListener('click', requestTrip);
@@ -83,20 +93,32 @@ function checkForData() {
   }
 }
 
-function login(event) {
-  event.preventDefault();
-
+function getLoginData() {
   const loginData = new FormData(loginForm);
-  const loginCredentials = [...loginData.values()];
+  return [...loginData.values()];
+}
+
+function verifyLogin() {
+  const loginCredentials = getLoginData();
   const loginID = parseInt(loginCredentials[0].split('traveler')[1]);
 
   if (loginID && loginCredentials[1] === 'travel') {
-    requestData(`/${loginID}`);
-    switchPages();
-    loginForm.reset();
+    loginBtn.disabled = false;
   } else {
-    alert('Nope');
+    loginBtn.disabled = true;
   }
+}
+
+function login(event) {
+  event.preventDefault();
+
+  const loginCredentials = getLoginData();
+  const loginID = parseInt(loginCredentials[0].split('traveler')[1]);
+
+  requestData(`/${loginID}`);
+  switchPages();
+  loginForm.reset();
+  loginBtn.disabled = true;
 }
 
 function switchPages() {
@@ -106,8 +128,28 @@ function switchPages() {
 }
 
 function displayUserInfo() {
+  const yearlyExpense = user.trips.reduce((acc, currTrip) => {
+    if (currTrip.date >= `${dayjs(currentDate).format('YYYY')}/1/1` && currTrip.date <= `${dayjs(currentDate).format('YYYY')}/12/31`) {
+      console.log("if");
+      acc += getCostEstimate(currTrip);
+    }
+    return acc;
+  }, 0);
+
   userNameDisplay.innerText = user.name;
-  yearlyExpenseDisplay.innerText = `$${user.calcYearExpenses(destinationsRepo, 2022)}`;
+  yearlyExpenseDisplay.innerText = yearlyExpense;
+}
+
+function getCostEstimate(trip) {
+  const location = destinationsRepo.findEntry('id', trip.id);
+
+  let total = (
+    (trip.travelers * location.estimatedFlightCostPerPerson) 
+    + (trip.duration * location.estimatedLodgingCostPerDay) 
+    * 1.1
+  );
+
+  return total;
 }
 
 function parseTripDates() {
@@ -117,16 +159,31 @@ function parseTripDates() {
 }
 
 function displayUserTrips() {
-  tripsSection.innerHTML = '';
+  userTripsDisplay.forEach(section => {
+    section.innerHTML = '';
+  });
+
   user.trips.forEach(trip => {
-    tripsSection.innerHTML += `
-    <article>
-      <p>Destination: <span class="trip-destination-display">${destinationsRepo.filterData('id', trip.destinationID)[0].destination}</span></p>
-      <p>Date: <span class="trip-date-display">${dayjs(trip.date).format('dddd, MMM D, YYYY')}</span></p>
-      <p>Duration: <span class="trip-duration-display">${trip.duration}</span> days</p>
-      <p>Status: <span class="trip-status-display">${trip.status}</span></p>
-    </article>
+    const plannedTrip = destinationsRepo.findEntry('id', trip.destinationID)
+    
+    const tripCard = `
+      <article class="trip-card">
+        <h3><span class="trip-destination-display">${plannedTrip.destination}</span></h3>
+        <img src="${plannedTrip.image}" alt="${plannedTrip.alt}" />
+        <p><span class="trip-date-display">${dayjs(trip.date).format('dddd, MMM D, YYYY')}</span></p>
+        <p><span class="trip-duration-display">${trip.duration}</span> day(s)</p>
+        <p>Status: <span class="trip-status-display">${trip.status}</span></p>
+      </article>
     `;
+
+    if (trip.date < currentDate) {
+      pastTripsSection.innerHTML += tripCard;
+    }
+    else if (trip.date > currentDate && trip.status === 'approved') {
+      futureTripsSection.innerHTML += tripCard;
+    } else {
+      pendingTripsSection.innerHTML += tripCard;
+    }
   });
 }
 
@@ -134,39 +191,73 @@ function populateLocationChoices() {
   locationChoices.innerHTML = '';
   destinationsRepo.data.forEach(location => {
     locationChoices.innerHTML += `
-    <option value="${location.id}">${location.destination}</option>
+      <option value="${location.id}">${location.destination}</option>
     `;
   })
 }
 
-function validateTripRequest() {
-  
+function getTripFormData() {
+  const tripDate = new FormData(requestTripForm);
+  const parsedTripDate = [];
+  tripDate.forEach(element => parsedTripDate.push(parseInt(element)));
+  return parsedTripDate;
+}
 
+function validateTripRequest() {
+  const tripFormData = getTripFormData();
+
+  if (tripFormData.every(element => element > 0) && dateInput.value) {
+    displayTripCost(tripFormData);
+    requestTripBtn.disabled = false;
+  } else {
+    requestTripBtn.disabled = true;
+    estimatedCostDisplay.innerText = 0;
+  }
+
+  displayLocationChoice(tripFormData);
+}
+
+function displayLocationChoice(tripFormData) {
+  const selectedDestination = destinationsRepo.findEntry('id', tripFormData[0]);
+  
+  destinationNameDisplay.innerText = selectedDestination.destination;
+  destinationImg.src = selectedDestination.image;
+  destinationImg.alt = selectedDestination.alt;
+  destinationLodgingDisplay.innerText = selectedDestination.estimatedLodgingCostPerDay;
+  destinationFlightDisplay.innerText = selectedDestination.estimatedFlightCostPerPerson;
+}
+
+function displayTripCost(tripFormData) {
+  const costEstimate = getCostEstimate({
+    id: tripFormData[0], 
+    duration: tripFormData[1], 
+    travelers: tripFormData[2]
+  });
+
+  estimatedCostDisplay.innerText = costEstimate;
 }
 
 function requestTrip(event) {
   event.preventDefault();
   
-  const tripRequest = new FormData(requestTripForm);
-  const values = [...tripRequest.values()];
-
+  const tripFormData = getTripFormData();
   const tripRequestData = {
     id: tripsRepo.data.length + 1,
     userID: user.id,
-    destinationID: parseInt(values[2]),
-    travelers: parseInt(values[1]),
-    date: dayjs(dateInput.value).format('YYYY/MM/D'), // Grab date selection
-    duration: parseInt(values[0]),
+    destinationID: tripFormData[0],
+    travelers: tripFormData[2],
+    date: dayjs(dateInput.value).format('YYYY/MM/D'),
+    duration: tripFormData[1],
     status: 'pending',
     suggestedActivities: []
   }
-
-  postData('trips', tripRequestData);
+  postData('trips', tripRequestData, user.id);
 
   requestTripForm.reset();
+  requestTripBtn.disabled = true;
 }
 
 export { requestData };
 
-// switchPages();
-// requestData('/50');
+switchPages();
+requestData('/50');
